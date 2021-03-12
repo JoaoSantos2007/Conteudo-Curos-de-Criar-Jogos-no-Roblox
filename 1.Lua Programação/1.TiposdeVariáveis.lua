d789f281093310c2b31afc67ac0f6c317a162bd4
João Pedro Tomaz dos Santos <80163527+JoaoSantos2007@users.noreply.github.com>
2021-03-12 14:52:43 -0300

    Create 1.TiposdeVariáveis.lua

---
+++ 1.Lua Programação/1.TiposdeVariáveis.lua
@@ -0,0 +1,24 @@
+--Hello
+
+local energia = 5
+local nome = 'João'
+local passoufase = false
+
+--Somando Valores
+local maisenergia = 2
+soma = energia+maisenergia
+
+--Concatenando Texto
+print("Nome do Jogador: "..nome)
+print("Energia do Jogador: "..energia.. "; Energia a adicionar: "..maisenergia.."; Energia Atual: "..energia+maisenergia)
+
+--VErificar Tipos de Variáveis
+print("O tipo da variável energia é: "..type(energia))
+print("O tipo da variável nome é: "..type(nome))
+print("O tipo da variável passoufase é: "..type(passoufase))
+
+--Operadores Relacionais
+print(energia == maisenergia)
+print(energia ~= maiseneregia)
+print(energia >= maisenergia)
+print(energia <= maiseneregia)
